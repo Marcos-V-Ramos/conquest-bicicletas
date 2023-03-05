f0a9cf151651744bba1f7ed0920fe9a4a537d8a4
Reidiney <reidineyanjos@gmail.com>
2023-03-05 12:49:24 -0300

    Start
    Estrutura e dependências definidas

--- src/main/java/com/conquestbicicletas/ProjectApplication.java
+++ src/main/java/com/conquestbicicletas/ProjectApplication.java
@@ -1,9 +1,10 @@
-package com.conquestbicicletas.project;
+package com.conquestbicicletas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ProjectApplication {
 
 	public static void main(String[] args) {
