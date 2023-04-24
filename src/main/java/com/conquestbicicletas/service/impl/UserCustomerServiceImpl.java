e93209b582ff575da2435ebcfb7404d6b0b382ce
Reidiney <reidineyanjos@gmail.com>
2023-04-24 00:05:24 -0300

    BackEnd Corrections

--- src/main/java/com/conquestbicicletas/service/impl/UserCustomerServiceImpl.java
+++ src/main/java/com/conquestbicicletas/service/impl/UserCustomerServiceImpl.java
@@ -2,10 +2,15 @@ package com.conquestbicicletas.service.impl;
 
 import org.springframework.stereotype.Component;
 
+import com.conquestbicicletas.service.UserCustomerService;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class UserCustomerServiceImpl {
+public class UserCustomerServiceImpl implements UserCustomerService{@Override
+	public boolean encherto() {
+		return false;
+	}
 
 }
